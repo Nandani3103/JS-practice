59b76bff26be8cd0a0e8c08fa2c1ae41ea65781f
Nandani Sharma <141529229+Nandani3103@users.noreply.github.com>
2024-07-19 21:24:24 +0000

    operations

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+//Conversion
 let score = "33"
 let a = true
 let b = 33
@@ -12,3 +13,39 @@ console.log(typeof a);
 letstringNumber = String(b)
 console.log(b);
 console.log(typeof letstringNumber);
+
+
+//Operation
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%3);
+
+let str1 = "Hello "
+let str2 = "Nandani"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+
+let num1 , num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
