453b29c4aedcac01f9b60eb9aed68b01501955f3
Nandani Sharma <141529229+Nandani3103@users.noreply.github.com>
2024-07-19 21:33:39 +0000

    comparision

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+
+console.log("2" > "1");
+console.log("02" > "1")
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//While comparision null is converted to 0 but in case of == its not the case
+console.log("2" === 2);
