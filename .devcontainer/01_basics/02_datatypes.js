fedca4b4421ac01be44917bf354ed3506c150344
Nandani Sharma <141529229+Nandani3103@users.noreply.github.com>
2024-07-19 20:57:44 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) we are using node js not browser
+
+
+let name = "Nandani" //String
+let age = 21 //integer
+let isLoggedIn = false //boolean
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => means a variable does not contain any value
+//symbol
+
+console.log(typeof age);
