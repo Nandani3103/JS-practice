65e49ed3face4ce8419d5e4ace02ba51ee5bf194
Nandani Sharma <141529229+Nandani3103@users.noreply.github.com>
2024-07-19 20:12:44 +0000

    variables

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,9 @@
+//after assigning a value to a const variable the value of the variable can't be changes i.e, it becomes constant
+const accountId = 144553
+//after assigning a value to let variable it follows the concept of scope
+let accountEmail = "nandanisharmavis@gmail.com"
+//var is generally not used because it can be changes within or outside the scope
+var accountPassword = "12345"
+accountCity = "Kolkata"
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
