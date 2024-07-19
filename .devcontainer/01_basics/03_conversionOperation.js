d254d2d8419b1363bbfa00bc5e158fdd30705578
Nandani Sharma <141529229+Nandani3103@users.noreply.github.com>
2024-07-19 21:12:06 +0000

    conversion of data types

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33"
+let a = true
+let b = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(typeof a);
+
+letstringNumber = String(b)
+console.log(b);
+console.log(typeof letstringNumber);
