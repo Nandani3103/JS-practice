ecf145f28faf9680b8527841fdb0decf7a9d3c08
Nandani Sharma <141529229+Nandani3103@users.noreply.github.com>
2024-09-08 21:53:56 +0530

    object 3

---
+++ .devcontainer/02_basics/05_objects3.js
@@ -0,0 +1,25 @@
+const course = {
+    coursename : 'js in hindi',
+    price : '999',
+    courseInstrutor : 'hitesh'
+}
+
+//course.courseInstrutor
+
+const { courseInstrutor : instructor} = course;
+
+// console.log(courseInstrutor);
+console.log(instructor);
+
+// const navbar = ({company}) => {
+
+// } 
+
+// navbar(company = "nandani")
+
+// API -------->
+//{
+//     "name" : 'nandani',
+//     "coursename" : "js in hindi",
+//     "price" : "Free"
+// }
